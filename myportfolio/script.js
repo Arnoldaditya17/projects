a139165e97a88db68d6cca52243f0a85fae009f4
Aditya singh Rajput <94311328+Arnoldaditya17@users.noreply.github.com>
2023-03-19 13:40:02 +0530

    Add files via upload

---
+++ myportfolio/script.js
@@ -0,0 +1,37 @@
+
+
+
+
+var typed = new typed("typing", {
+  Strings: ["freelancer", "Ethicalhacker"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  centeredSlides: false,
+  slidesPerGroupSkip: 1,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
